ecb1002c463f015fb3b31b1a91885ab5da3f2819
William Dunk <williamdunk@gmail.com>
2019-02-08 15:34:43 +0000

    styled it out

--- interface.js
+++ interface.js
@@ -21,23 +21,48 @@ $(document).ready(function() {
 
   $('#psm-on').click(function() {
     thermostat.switchPowerSavingModeOn();
-    $('#power-saving-status').text('on')
+    $('#power-saving-status').text('ON')
     updateTemperature();
   })
 
   $('#psm-off').click(function() {
     thermostat.switchPowerSavingModeOff();
-    $('#power-saving-status').text('off')
+    $('#power-saving-status').text('OFF')
     updateTemperature();
   })
-
-  // function updateTemperature() {
-  //   $('#temperature').text(thermostat.temperature);
-  // }
 
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.energyUsage());
+  }
+
+  // $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  //   $('#current-temperature').text(data.main.temp);
+  // })
+
+  displayWeather('london');
+
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    displayWeather(city)
+    $(document.body).attr('class', city);
+  })
+
+  // displayWeather('London');
+  //
+  // $('#select-city').submit(function(event) {
+  //   event.preventDefault();
+  //   var city = $('#current-city').val();
+  //   displayWeather(city);
+  // })
+
+  function displayWeather(city) {
+     var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+     var token = '&appid=' + '';
+     var units = '&units=metric';
+     $.get(url + token + units, function(data) {
+       $('#current-temperature').text(data.main.temp);
+     })
   }
 
 })
